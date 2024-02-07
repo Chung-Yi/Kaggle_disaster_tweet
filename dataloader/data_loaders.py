0e97a4e70d80d7ac0d2f0051bbc2589dd21e8ba6
wty81213 <wty81213@hotmail.com>
2024-02-07 20:34:30 +0800

    fixed dataloader

--- dataloader/data_loaders.py
+++ dataloader/data_loaders.py
@@ -1,10 +1,10 @@
 from .base_dataloader import BaseDataLoader
-from costom_dataset import DisasterDataset
+from costom_dataset import DisasterDataset, DisasterTweetDataset
 
 class DisatserDataLoader(BaseDataLoader):
     def __init__(self, config):
 
-        dataset = DisasterDataset('train', config)
+        dataset = DisasterTweetDataset(config, 'train')
         super().__init__(dataset, int(config['data_loader']['batch_size']),float(config['data_loader']["split_weight"]), int(config['data_loader']['num_workers']))
 
         
